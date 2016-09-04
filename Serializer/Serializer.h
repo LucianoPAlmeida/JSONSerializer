64df678ef71512815f4cfcb369012306532bdaea
LucianoAlmeida <passos.luciano@outlook.com>
2016-09-03 21:38:06 -0300

    Initial Commit

---
+++ Serializer/Serializer.h
@@ -0,0 +1,19 @@
+//
+//  Serializer.h
+//  Serializer
+//
+//  Created by Luciano Almeida on 9/3/16.
+//  Copyright © 2016 Luciano Almeida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Serializer.
+FOUNDATION_EXPORT double SerializerVersionNumber;
+
+//! Project version string for Serializer.
+FOUNDATION_EXPORT const unsigned char SerializerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Serializer/PublicHeader.h>
+
+
---
+++ SerializerTests/SerializerTests.swift
@@ -0,0 +1,36 @@
+//
+//  SerializerTests.swift
+//  SerializerTests
+//
+//  Created by Luciano Almeida on 9/3/16.
+//  Copyright © 2016 Luciano Almeida. All rights reserved.
+//
+
+import XCTest
+@testable import Serializer
+
+class SerializerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
